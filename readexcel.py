6f4afa217c9c5cd3a1b718a0153dad76815882ed
jack345466 <72225376+jack345466@users.noreply.github.com>
2020-11-09 14:31:53 -0500

    Update readexcel.py

--- readexcel.py
+++ readexcel.py
@@ -2,9 +2,9 @@
 import csv
 import bpy
 with open ("fy20_adc_data_file_88_degrees.csv", newline="") as lunar_data:
-	for row in csv.reader(lunar_data):
-		latitude = row[0];
-		longitude = row[1];
-		height = row[2];
-		slope = row[3];
-		bpy.ops.mesh.primitive_sphere_add(location=(latitude, longitude, height));
+    for row in csv.reader(lunar_data):
+        latitude = (float(row[0])+88)*1850;
+        longitude = (float(row[1])+88)*1850;
+        height = (float(row[2])+88)*1850;
+        slope = row[3];
+        bpy.ops.mesh.primitive_sphere_add(location=(latitude, longitude, height));
