bdeb9e0ed2208e4190b8e8c0bc492b5ac713932d
jack345466 <72225376+jack345466@users.noreply.github.com>
2020-11-13 16:02:55 -0500

    Update elevationandazimuth.py

--- elevationandazimuth.py
+++ elevationandazimuth.py
@@ -1,3 +1,11 @@
 import math;
 def getAzimuth(latA, longA, latB, longB):
-    azimuth = atan2(sin(longB-longA) * cos(latB), (cos(latA)*sin(latB)) - (sin(latA) * cos(latB) * cos(longB - longA)) );
+    azimuth = atan2(math.sin(longB-longA) * math.cos(latB), (math.cos(latA)*math.sin(latB)) - (math.sin(latA) * math.cos(latB) * math.cos(longB - longA)) );
+    return azimuth;
+
+def getElevation(xa, ya, za, xb, yb, zb):
+    Xab = xb - xa;
+    Yab = yb - ya;
+    Zab = zb - za;
+    range = sqrt(Xab**2 + Yab**2 + Zab**2);
+    rz = 
