954e5b82d1f748ef5db0cc03f907d7320866fa24
jack345466 <72225376+jack345466@users.noreply.github.com>
2020-11-13 16:50:36 -0500

    Update elevationandazimuth.py

--- elevationandazimuth.py
+++ elevationandazimuth.py
@@ -8,13 +8,16 @@ def getElevation(xa, ya, za, xb, yb, zb):
     Yab = yb - ya;
     Zab = zb - za;
     if (Xab < 0 || Yab < 0 || Zab < 0):
-        print("Input error!");
+        print("Input error!");              #The square root of a negative number doesn't exist.
         return -1;
     Range = math.sqrt(Xab**2 + Yab**2 + Zab**2);
     rz = Xab * math.cos(ya) * math.cos(xa) + Yab * math.cos(ya) * math.sin(xa) + Zab * math.sin(ya);
     elevation = math.asin(rz/Range);
     return elevation;
 
+checkpoints = [];
 if ( getAzimuth(moon to earth) == getAzimuth(moon to horizon) ):
     if ( getElevation(to earth) > getElevation(to horizon) ):
-        ding ding ding
+        checkpoints.append();
+        
+checkpoints.sort(key=lambda x: x.max());
